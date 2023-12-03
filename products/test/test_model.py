c407e60284d994b24045dc9c982d490a963934d0
JungHoon99 <greenjm1@nate.com>
2023-12-03 14:34:25 +0900

    ADD update, delete test code

--- products/test/test_model.py
+++ products/test/test_model.py
@@ -5,13 +5,26 @@ class ModelTestCase(TestCase):
     """ 이 클래스는 bucketlist 모델을 위한 test suite를 정의합니다."""
     def setUp(self):
         """ 테스트 클라이언트와 기타 테스트 변수를 정의합니다."""
-        self.bucketlist_name = "Write world class code"
+        self.product_name = "shirt"
         self.price = 5000
-        self.bucketlist = Product(name=self.bucketlist_name, price=self.price)
+        self.product = Product.objects.create(name=self.product_name, price=self.price)
 
-    def test_model_can_create_a_bucketlist(self):
+    def test_model_create_product(self):
         """ bucketlist 모델을 테스트하면 bucketlist이 생성될 수 있습니다."""
+        new_product = Product(name=self.product_name+"1", price=self.price)
         old_count = Product.objects.count()
-        self.bucketlist.save()
+        self.product = new_product.save()
         new_count = Product.objects.count()
         self.assertNotEqual(old_count, new_count)
+
+    def test_model_update_product(self):
+        self.product.price = 10000
+        self.product.save()
+        update_product = Product.objects.get(id=self.product.id)
+        self.assertEqual(update_product.price, 10000)
+
+    def test_model_delete_product(self):
+        product_id = self.product.id
+        self.product.delete()
+        with self.assertRaises(Product.DoesNotExist):
+            deleted_product = Product.objects.get(id=product_id)
