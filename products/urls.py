8a3a511c9f1dee5b6be90a7e030b90c698f91f76
JungHoon99 <greenjm1@nate.com>
2023-12-03 16:57:56 +0900

    url add

---
+++ products/urls.py
@@ -0,0 +1,9 @@
+﻿from django.urls import path
+
+from products.views import Product_listAPI, Product_detailAPI
+
+
+urlpatterns = [
+    path('', Product_listAPI, name='product_list'),
+    path('<int:id>/', Product_detailAPI, name='product_detail'),
+]
