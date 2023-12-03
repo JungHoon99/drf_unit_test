78ccc258c5b5820b4ea998a1ac4b5c68b3829756
JungHoon99 <greenjm1@nate.com>
2023-12-03 16:46:59 +0900

    product view 구현

--- products/views.py
+++ products/views.py
@@ -1,5 +1,21 @@
 from rest_framework.viewsets import ModelViewSet
+
+from products.serializers import ProductSerializer
+from products.models import Product
 # Create your views here.
 
 class ProductViewSet(ModelViewSet):
-    
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+
+Product_listAPI = ProductViewSet.as_view({
+    'get': 'list',
+    'post': 'create',
+})
+
+Product_detailAPI = ProductViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'delete': 'destroy',
+})
