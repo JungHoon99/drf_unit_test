f78ee3b50d33c886e28cbc21b4fa49cb31889aa3
JungHoon99 <greenjm1@nate.com>
2023-12-03 17:14:25 +0900

    ADD product list view test code

--- products/test/test_model.py
+++ products/test/test_model.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from products.models import Product
 
-class ModelTestCase(TestCase):
+class ProductModelTestCase(TestCase):
     def setUp(self):
         # 기본적인 변수 선언
         self.product_name = "shirt"
@@ -9,7 +9,7 @@ class ModelTestCase(TestCase):
         self.product = Product.objects.create(name=self.product_name, 
                                               price=self.price)
 
-    def test_model_create_product(self):
+    def test_product_create(self):
         # 새로운 제품 데이터 생성후 데이터 베이스에서 추가 확인
         new_product = Product(name=self.product_name+"1", price=self.price)
         old_count = Product.objects.count()
@@ -17,7 +17,7 @@ class ModelTestCase(TestCase):
         new_count = Product.objects.count()
         self.assertNotEqual(old_count, new_count)
 
-    def test_model_update_product(self):
+    def test_product_update(self):
         # 제품 수정후 데이터 수정 여부와 수정시간 변화 확인
         self.product.price = 10000
         defore_modify_datetime = self.product.date_modified
@@ -27,7 +27,7 @@ class ModelTestCase(TestCase):
         self.assertEqual(update_product.price, 10000)
         self.assertNotEqual(defore_modify_datetime, after_modify_datetime)
 
-    def test_model_delete_product(self):
+    def test_product_delete(self):
         #데이터 삭제 후 데이터 확인
         product_id = self.product.id
         self.product.delete()
---
+++ products/test/test_view.py
@@ -0,0 +1,21 @@
+﻿from rest_framework.test import APITestCase, APIRequestFactory
+from rest_framework.status import HTTP_200_OK
+from django.urls import reverse
+
+
+from products.views import Product_listAPI
+
+
+
+class ProductAPITestCase(APITestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.view = Product_listAPI
+        self.url=reverse('product_list')
+
+    def test_product_list_view(self):
+        request = self.factory.get(self.url)
+
+        response = self.view(request)
+
+        self.assertEqual(response.status_code, HTTP_200_OK)
