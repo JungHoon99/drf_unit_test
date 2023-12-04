4c8f51d51092e0eb3ce776f1e35588cf605e4b03
JungHoon99 <greenjm1@nate.com>
2023-12-04 18:32:59 +0900

    ADD user update delete test code

--- accounts/test/test_models.py
+++ accounts/test/test_models.py
@@ -5,20 +5,18 @@ from accounts.test.factorys import UserFactory
 
 class MyUserModelTestCase(TestCase):
     def setUp(self):
-        self.user = UserFactory.build()
+        self.user = UserFactory.create()
 
-    def test_create(self):
-        # 새로운 제품 데이터 생성후 데이터 베이스에서 추가 확인
-        new_user = MyUser(
-            userid=self.user.userid,
-            name=self.user.name,
-            email=self.user.email,
-            phone=self.user.phone,
-            password=self.user.password
-        )
+    def test_update(self):
+        # 제품 수정후 데이터 수정 여부와 수정시간 변화 확인
+        self.user.name = "테스터"
+        self.user.save()
+        update_user = MyUser.objects.get(id=self.user.id)
+        self.assertEqual(update_user.name, "테스터")
 
-        old_count = MyUser.objects.count()
-        self.product = new_user.save()
-        new_count = MyUser.objects.count()
-
-        self.assertNotEqual(old_count, new_count)
+    def test_delete(self):
+        #데이터 삭제 후 데이터 확인
+        user_id = self.user.id
+        self.user.delete()
+        with self.assertRaises(MyUser.DoesNotExist):
+            deleted_product = MyUser.objects.get(id=user_id)
