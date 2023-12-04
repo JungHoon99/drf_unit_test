0c73956d3eb570a0a699eb121db0f6663ac53846
JungHoon99 <greenjm1@nate.com>
2023-12-04 10:09:19 +0900

    ADD product detail test case

--- products/test/test_views.py
+++ products/test/test_views.py
@@ -1,8 +1,8 @@
-﻿from rest_framework.test import APITestCase, APIRequestFactory
-from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
+﻿from rest_framework.test import APITestCase, APIRequestFactory, APIClient
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT
 from django.urls import reverse
 
-
+from products.models import Product
 from products.views import Product_listAPI
 
 
@@ -26,6 +26,39 @@ class ProductListAPITestCase(APITestCase):
 
         response = self.view(request)
 
-        print(response.data)
-
         self.assertEqual(response.status_code, HTTP_201_CREATED)
+
+
+class productDetailAPITestCase(APITestCase):
+    def setUp(self):
+        self.product_name = "shirt"
+        self.price = 5000
+        self.product = Product.objects.create(name=self.product_name, price=self.price)
+        self.client = APIClient()
+
+    def test_get_product(self):
+        # 제품을 가져오는 GET 요청 테스트
+        response = self.client.get(f'/product/{self.product.id}/')
+
+        self.assertEqual(response.status_code, HTTP_200_OK)
+        self.assertEqual(response.data['name'], self.product_name)
+
+    def test_update_product(self):
+        # 제품을 업데이트하는 PUT 요청 테스트
+        updated_data = {'name': '업데이트된 제품', 'price': 150}
+        response = self.client.put(f'/product/{self.product.id}/', updated_data, format='json')
+
+        self.assertEqual(response.status_code, HTTP_200_OK)
+
+        updated_product = Product.objects.get(id=self.product.id)
+        self.assertEqual(updated_product.name, '업데이트된 제품')
+        self.assertEqual(updated_product.price, 150)
+
+    def test_delete_product(self):
+        # 제품을 삭제하는 DELETE 요청 테스트
+        response = self.client.delete(f'/product/{self.product.id}/')
+
+        self.assertEqual(response.status_code, HTTP_204_NO_CONTENT)
+
+        with self.assertRaises(Product.DoesNotExist):
+            deleted_product = Product.objects.get(id=self.product.id)
--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,5 @@ from products.views import Product_listAPI, Product_detailAPI
 
 urlpatterns = [
     path('', Product_listAPI, name='product_list'),
-    path('<int:id>/', Product_detailAPI, name='product_detail'),
+    path('<int:pk>/', Product_detailAPI, name='product_detail'),
 ]
