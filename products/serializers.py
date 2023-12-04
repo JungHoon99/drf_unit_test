589faa8feadc1396eb83343972c4d8c9535577fa
JungHoon99 <greenjm1@nate.com>
2023-12-04 10:26:07 +0900

    add price validate

--- products/serializers.py
+++ products/serializers.py
@@ -1,4 +1,4 @@
-﻿from rest_framework.serializers import ModelSerializer
+﻿from rest_framework.serializers import ModelSerializer, ValidationError
 
 from products.models import Product
 
@@ -8,3 +8,8 @@ class ProductSerializer(ModelSerializer):
         model = Product
         fields='__all__'
         read_only_fields = ('id', 'date_created', 'date_modified')
+
+    def validate_price(self, value):
+        if value < 0:
+            raise ValidationError("가격은 음수일 수 없습니다.")
+        return value
