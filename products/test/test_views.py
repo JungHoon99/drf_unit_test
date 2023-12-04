48305783e0993cadd4619496b33405709a193b2e
JungHoon99 <greenjm1@nate.com>
2023-12-04 11:27:13 +0900

    APITestCase  code  변환

--- products/test/test_views.py
+++ products/test/test_views.py
@@ -8,23 +8,20 @@ from products.views import Product_listAPI
 
 class ProductListAPITestCase(APITestCase):
     def setUp(self):
-        self.factory = APIRequestFactory()
-        self.view = Product_listAPI
         self.url=reverse('product_list')
 
-    def test_product_list_view(self):
-        request = self.factory.get(self.url)
-
-        response = self.view(request)
+    def test_list_view(self):
+        response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, HTTP_200_OK)
 
     def test_product_create_view(self):
         sample_post={'name': 'pants', 'price': 67000}
 
-        request = self.factory.post(self.url, sample_post)
-
-        response = self.view(request)
+        response = self.client.post(
+            self.url, 
+            sample_post
+            )
 
         self.assertEqual(response.status_code, HTTP_201_CREATED)
 
@@ -36,14 +33,14 @@ class productDetailAPITestCase(APITestCase):
         self.product = Product.objects.create(name=self.product_name, price=self.price)
         self.client = APIClient()
 
-    def test_get(self):
+    def test_retrieve_view(self):
         # 제품을 가져오는 GET 요청 테스트
         response = self.client.get(f'/product/{self.product.id}/')
 
         self.assertEqual(response.status_code, HTTP_200_OK)
         self.assertEqual(response.data['name'], self.product_name)
 
-    def test_update(self):
+    def test_update_view(self):
         # 제품을 업데이트하는 PUT 요청 테스트
         updated_data = {'name': '업데이트된 제품', 'price': 150}
         response = self.client.put(f'/product/{self.product.id}/', updated_data, format='json')
@@ -54,7 +51,7 @@ class productDetailAPITestCase(APITestCase):
         self.assertEqual(updated_product.name, '업데이트된 제품')
         self.assertEqual(updated_product.price, 150)
 
-    def test_delete(self):
+    def test_destroy_view(self):
         # 제품을 삭제하는 DELETE 요청 테스트
         response = self.client.delete(f'/product/{self.product.id}/')
 
